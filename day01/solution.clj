00e5998afd016339b0aa65951fb9c5534cf813a2
Piotr Kaznowski <piotr@kazno.dev>
2022-12-01 23:50:48 +0100

    Day 1

---
+++ day01/solution.clj
@@ -0,0 +1,38 @@
+(ns day01.solution
+  (:require [tools :refer [file->lines]]))
+
+(defn parse-input [lines]
+  (->> lines
+       (partition-by empty?)
+       (take-nth 2)
+       (map #(map parse-long %))
+       (map #(apply + %))
+       (sort >)
+       (take 3)))
+
+(defn -main [day]
+  (let [input (parse-input (file->lines day))]
+    {:part1 (first input)
+     :part2 (apply + input)}))
+
+
+(comment
+  (require '[clojure.string :refer [split]])
+  (let [test-str "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"
+        input (parse-input (split test-str #"\n"))]
+    {:part1 (= (first input) 24000)
+     :part2 (= (apply + input) 45000)})
+  )
