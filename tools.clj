5998b90340fed37fd36e13fc1837b13bd13b8046
Piotr Kaznowski <piotr@kazno.dev>
2022-12-01 23:49:41 +0100

    Simplified tools, better solving task

--- tools.clj
+++ tools.clj
@@ -1,7 +1,8 @@
 (ns tools
   (:require [clojure.string :refer [split]]))
 
-(defn inp->lines [dir {:keys [fname splitby]}]
-  (-> (str dir "/" (or fname "input.txt"))
-      slurp
-      (split (re-pattern splitby))))
+
+(defn file->lines [dir]
+  (-> (str dir "/" "input.txt")
+       slurp
+       (split #"\n")))
